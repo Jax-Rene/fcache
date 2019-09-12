e6ad989e850a19e379e89b29f44c77f25956acb3
zhuangjy <249602015@qq.com>
2019-09-12 17:22:30 +0800

    Netty Core Code

---
+++ src/main/java/me/zhuangjy/core/Bootstrap.java
@@ -0,0 +1,34 @@
+package me.zhuangjy.core;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class Bootstrap {
+
+    private static final int PORT = 8897;
+
+    public static void main(String[] args) throws Exception {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new HttpStaticFileServerInitializer());
+
+            Channel ch = b.bind(PORT).sync().channel();
+            System.err.println("Open your web browser and navigate to http://127.0.0.1:" + PORT + '/');
+            ch.closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+
+}
